56b5f75fd058219610e5b027dc4ec34c6ce5bb2f
matgrand <matteo.grandin@gmail.com>
2024-05-24 15:02:38 +0200

    d

--- explore_mdsplus.py
+++ explore_mdsplus.py
@@ -1,6 +1,7 @@
 # print tree structure
 import MDSplus as mds
 import os
+import random
 
 print('MDSplus version:', mds.__version__)
 
@@ -24,19 +25,24 @@ def traverse_tree(node, level=0):
 
 # do the same but without recursion
 def traverse_tree2(head_node, max_depth=10):
+    # define max_depth random terminal colors
+    colors = ['\e[38;5;{}m'.format(random.randint(0, 255)) for _ in range(max_depth)]
+    ec = '\033[0m' # end color
+    # for _ in range(max_depth):
+    #     colors.append('\e[38;5;{}m'.format(random.randint(0, 255)))
     curr_nodes = [head_node]
     for d in range(max_depth):
         print('Depth:', d)
         next_nodes = []
         for node in curr_nodes:
-            print("   " * d + node.node_name)
+            print(colors[d] + "   " * d + node.node_name + ec)
 
             # get data in the node (if any)
             try:
                 data = node.data 
-                print("   " * d + str(data))
+                print(colors[d] + "   " * d + str(data) + ec)
             except:
-                print("   " * d + "_____________")
+                print(colors[d] + "   " * d + "_____________" + ec)
 
             # get the children of the node
             try:
