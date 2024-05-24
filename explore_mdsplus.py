8a319e5967bbc5efe78607b736214d0f0baf8176
matgrand <matteo.grandin@gmail.com>
2024-05-24 12:18:28 +0200

    +

--- explore_mdsplus.py
+++ explore_mdsplus.py
@@ -2,6 +2,8 @@
 import MDSplus as mds
 import os
 
+rfx = mds.Tree('rfx', 30810, 'readonly') # open the tree
+
 def traverse_tree(node, level=0):
     print('  ' * level + node.node_name) # print the name of the node 
     children = node.getChildren() # get the children of the node
@@ -11,7 +13,17 @@ def traverse_tree(node, level=0):
         except: pass
     for member in members:
         try: traverse_tree(member, level + 1)
-        except: pass
-        
-rfx = mds.Tree('rfx', 30810, 'readonly')
-traverse_tree(rfx.getNode('\\TOP')) # start the traversal at the top node
+        except: pass        
+
+# traverse_tree(rfx.getNode('\\TOP')) # start the traversal at the top node
+
+# do the same but without recursion
+MAX_DEPTH = 3
+curr_nodes = [rfx.getNode('\\TOP')]
+for d in range(MAX_DEPTH):
+    print('Depth:', d)
+    next_nodes = []
+    for node in curr_nodes:
+        print(node.node_name)
+        next_nodes += node.getChildren() + node.getMembers()
+    curr_nodes = next_nodes
