6dcbe5c44e79a9ca75af66f318127b3bde2d8a4d
matgrand <matteo.grandin@gmail.com>
2024-05-24 15:36:19 +0200

    d

--- explore_mdsplus.py
+++ explore_mdsplus.py
@@ -16,16 +16,18 @@ head_node = rfx.getNode('\\TOP.RFX') # get the top node
 COLORS = ['\033[38;5;{}m'.format(random.randint(0, 231)) for _ in range(MAX_DEPTH)]
 ENDC = '\033[0m'
 
-def traverse_tree(node, level=0):
+def traverse_tree(node, level=0, path=''):
     if level >= MAX_DEPTH: return # stop if the maximum depth is reached
-    print(COLORS[level] + '   ' * level + node.node_name + ENDC) # print the name of the node 
+    path = path + COLORS[level] + node.node_name + ENDC
+    # print(COLORS[level] + '   ' * level + node.node_name + ENDC) # print the name of the node 
+    print(path)
     children = node.getChildren() # get the children of the node
     members = node.getMembers() # get the members of the node
     for child in children:
-        try: traverse_tree(child, level + 1)
+        try: traverse_tree(child, level + 1, path)
         except: pass
     for member in members:
-        try: traverse_tree(member, level + 1)
+        try: traverse_tree(member, level + 1, path)
         except: pass        
 
 traverse_tree(head_node) # start the traversal at the top node
