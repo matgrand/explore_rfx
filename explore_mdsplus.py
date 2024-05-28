5fc1ea661c305b03a77a8fcf4e61564cc3aa8fe5
Grandin Matteo <grandin@rat2.rfx.local>
2024-05-28 18:24:01 +0200

    +

--- explore_mdsplus.py
+++ explore_mdsplus.py
@@ -5,7 +5,7 @@ import random
 
 print('MDSplus version:', mds.__version__)
 
-MAX_DEPTH = 4 # maximum depth to traverse the tree
+MAX_DEPTH = 9 # maximum depth to traverse the tree
 
 rfx = mds.Tree('rfx', 30810, 'readonly') # open the tree read-only
 # rfx = mds.Tree('rfx', 30810) # open the tree
@@ -14,7 +14,7 @@ rfx = mds.Tree('rfx', 30810, 'readonly') # open the tree read-only
 head_node = rfx.getNode('\\TOP.RFX') # get the top node
 
 #color the terminal output
-COLORS = ['\033[38;5;{}m'.format(random.randint(0, 231)) for _ in range(MAX_DEPTH)]
+COLORS = ['\033[38;5;{}m'.format(random.randint(8, 230)) for _ in range(MAX_DEPTH)]
 ENDC = '\033[0m'
 
 
@@ -26,21 +26,23 @@ def print_node(node, preprint=''):
     except Exception as e:
         print(preprint + node.node_name + 'ERR:' + str(e))
 
-def traverse_tree(node, level=0, path=''):
+def traverse_tree(node, level=0, path='', node_type='child'):
     if level >= MAX_DEPTH: return # stop if the maximum depth is reached
+    if node_type == 'child': node_name = node.node_name.upper()
+    elif node_type == 'member': node_name = node.node_name.lower()
+    else: raise ValueError('node_type must be either "child" or "member"') 
     path = path + '/' + COLORS[level] + node.node_name + ENDC
-    # print(COLORS[level] + '   ' * level + node.node_name + ENDC) # print the name of the node 
-    print(path)
+    # print(path)
     print_node(node, path)
     
     # go through the children and members of the node
     children = node.getChildren() # get the children of the node
     members = node.getMembers() # get the members of the node
     for child in children:
-        try: traverse_tree(child, level + 1, path)
+        try: traverse_tree(child, level + 1, path, 'child')
         except: pass
     for member in members:
-        try: traverse_tree(member, level + 1, path)
+        try: traverse_tree(member, level + 1, path, 'member')
         except: pass        
 
 # do the same but without recursion
@@ -60,7 +62,6 @@ def traverse_tree2(head_node):
                     next_nodes.append(child)
             except: pass
         curr_nodes = next_nodes
-
 
 if __name__ == '__main__':
 
